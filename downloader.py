97a210770dace0f2c7ee3cbb898eb46939bdf148
Abhishek Swain <abhiswain.it.2016@gmail.com>
2023-08-14 01:31:41 +0530

    Changed from AWS S3 to Google Drive

--- downloader.py
+++ downloader.py
@@ -1,6 +1,5 @@
 from pathlib import Path
-from argparse import ArgumentParser
-from wget import download
+import gdown
 
 
 def create_dirs():
@@ -9,36 +8,31 @@ def create_dirs():
     if not Path("models").exists():
         Path("models").mkdir(exist_ok=True)
 
-    return True
-
 
 def download_files():
-    if create_dirs():
-        print("'assets' & 'models' directories have been created")
+    create_dirs()
+    print("'assets' & 'models' directories have been created")
 
-        asset_urls = [
-            "https://search-engine-files.s3.us-east-2.amazonaws.com/assets/annoy_word_embedding_index.ann",
-            "https://search-engine-files.s3.us-east-2.amazonaws.com/assets/idx2word.json",
-            "https://search-engine-files.s3.us-east-2.amazonaws.com/assets/resnet50V2_image_embeds_index.ann",
-            "https://search-engine-files.s3.us-east-2.amazonaws.com/assets/word2idx.json",
-        ]
-        model_url = "https://search-engine-files.s3.us-east-2.amazonaws.com/models/resnet50v2_model-30-0.80.hdf5"
+    asset_url = (
+        "https://drive.google.com/drive/u/0/folders/1cBWJDINmH6N3MzOqDxmBzeAQW4oL3mlI"
+    )
+    model_url = (
+        "https://drive.google.com/drive/u/0/folders/13ce_FgTkDkZ3gxbjLaCEmp4ulH5lOjNK"
+    )
 
-        print("Downlaoding the assets now, please be patient, this may take sometime.")
-        # Download all the assets
-        for url in asset_urls:
-            download(url=url, out=f"assets\{url.split('/')[-1]}")
+    print("Downlaoding the assets now, please be patient, this may take sometime.")
 
-        print(
-            "Downloaded the assets they are present under 'assets' folder, now dowloading the model weights...."
-        )
-        download(url=model_url, out="models\resnet50v2_model-30-0.80.hdf5")
+    # Download all the assets
+    gdown.download_folder(url=asset_url, output="assets")
+    print(
+        "Downloaded the assets they are present under 'assets' folder, now dowloading the model weights...."
+    )
 
-        print(
-            "Model weights have been dowload and are present under 'models\resnet50v2_model-30-0.80.hdf5'"
-        )
+    gdown.download_folder(url=model_url, output="models")
+    print(
+        "Model weights have been dowload and are present under 'models\resnet50v2_model-30-0.80.hdf5'"
+    )
 
 
 if __name__ == "__main__":
-    create_dirs()
     download_files()
